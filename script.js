2f69ecab8207aa2d9d9e71cd0b95042b2cbf3d90
Filipe Oliveira <124939256+filipeoliveira7@users.noreply.github.com>
2023-03-21 11:42:58 +0000

    Create function operate

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function divide(num1, num2) {
 let firstNumber = 3;
 let operator = "+";
 let secondNumber = 5;
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  } else {
+    return "Error: Invalid operator";
+  }
+}
+
